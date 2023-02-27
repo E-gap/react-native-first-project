dc7a4266477a5e9b40a0b08c07b585f312c361c6
E-gap <106596181+E-gap@users.noreply.github.com>
2023-02-27 16:36:28 +0200

    update

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your!</Text>
+      <Text>E-gap react-native-first-project!</Text>
       <StatusBar style="auto" />
     </View>
   );
