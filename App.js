3ebeaebfaf7d2df1b84eabde004d8c183bd60cef
E-gap <106596181+E-gap@users.noreply.github.com>
2023-03-17 20:02:14 +0200

    update before 5 hw

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-//import { useRoute } from "./router";
-import { useRoute } from "./routertest";
+import { useRoute } from "./router";
+//import { useRoute } from "./routertest";
 
 export default function App() {
   const routes = useRoute(true);
--- router.js
+++ router.js
@@ -1,55 +1,16 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-  Platform,
-  Image,
-  KeyboardAvoidingView,
-  Keyboard,
-  TouchableWithoutFeedback,
-  Button,
-} from "react-native";
 import React from "react";
+import { StyleSheet } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-const AuthStack = createNativeStackNavigator();
 const MainStack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
 
 import RegistrationScreen from "./screens/RegistrationScreen";
-import Test1 from "./screens/Test1";
-import Test2 from "./screens/Test2";
 import LoginScreen from "./screens/LoginScreen";
-import PostsScreen from "./screens/PostsScreen";
-import CreatePostsScreen from "./screens/CreatePostsScreen";
-import ProfileScreen from "./screens/ProfileScreen";
 import Home from "./screens/Home";
 import MapScreen from "./screens/MapScreen";
 import CommentsScreen from "./screens/CommentsScreen";
 
-import { Feather } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
-
-export const useRoute = (isAuth) => {
-  /* if (!isAuth) {
-    return (
-      <AuthStack.Navigator>
-        <AuthStack.Screen
-          options={{ headerShown: false }}
-          name="LoginScreen"
-          component={LoginScreen}
-        />
-        <AuthStack.Screen
-          options={{ headerShown: false }}
-          name="RegistrationScreen"
-          component={RegistrationScreen}
-        />
-      </AuthStack.Navigator>
-    );
-  } */
+export const useRoute = () => {
   return (
     <MainStack.Navigator>
       <MainStack.Screen
